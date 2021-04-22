0bea3efc2da6aeab052437462fc656a65b79d398
mikh-ana <79360764+mikh-ana@users.noreply.github.com>
2021-04-22 19:14:02 +0300

    Добавьте файлы проекта.

---
+++ game/sapper.cs
@@ -0,0 +1,22 @@
+﻿using Miner.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class sapper : Form
+    {
+        public sapper()
+        {
+            InitializeComponent();
+            MapController.Init(this);
+        }
+    }
+}
